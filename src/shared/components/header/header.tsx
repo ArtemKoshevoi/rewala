652bcd659ca840d87eaada2a93e88b173c77728c
ArtemKoshevoi <artem.kos@2muchcoffee.com>
2019-08-20 16:06:04 +0300

    refactor(src/shared/components/header) change to FC

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -4,30 +4,28 @@ import { Button, ButtonGroup, Grid } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import { gridStyle } from './style';
 
-class Header extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <Grid item={true} style={gridStyle}>
-        <ButtonGroup
-          fullWidth={true}
-          variant='contained'
-        >
-          <Button component={NavLink} to='/' exact={true} activeStyle={{color: 'orange'}}>
-            Home
-          </Button>
-          <Button component={NavLink} to='/search' activeStyle={{color: 'orange'}}>
-            Search
-          </Button>
-          <Button component={NavLink} to='/notifications' activeStyle={{color: 'orange'}}>
-            Notifications
-          </Button>
-          <Button component={NavLink} to='/profile' activeStyle={{color: 'orange'}}>
-            Profile
-          </Button>
-        </ButtonGroup>
-      </Grid>
-    );
-  }
-}
+const Header: React.FC = () => {
+  return (
+    <Grid item={true} style={gridStyle}>
+      <ButtonGroup
+        fullWidth={true}
+        variant='contained'
+      >
+        <Button component={NavLink} to='/' exact={true} activeStyle={{color: 'orange'}}>
+          Home
+        </Button>
+        <Button component={NavLink} to='/search' activeStyle={{color: 'orange'}}>
+          Search
+        </Button>
+        <Button component={NavLink} to='/notifications' activeStyle={{color: 'orange'}}>
+          Notifications
+        </Button>
+        <Button component={NavLink} to='/profile' activeStyle={{color: 'orange'}}>
+          Profile
+        </Button>
+      </ButtonGroup>
+    </Grid>
+  );
+};
 
 export default Header;
